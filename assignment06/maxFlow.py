8d4af6b5d6d2247266d9ca1f5cfc16b60d338578
Xinya Yang <xinyayang0506@gmail.com>
2019-12-02 19:19:02 -0600

    dot

--- assignment06/maxFlow.py
+++ assignment06/maxFlow.py
@@ -4,17 +4,19 @@ from random import randint
 import networkx as nx
 import matplotlib.pyplot as plt
 
-def flow_network_generator():
-    #%%
+# def flow_network_generator():
+#%%
     G = nx.DiGraph(); 
 
     num_Nodes = randint(1, 10); #magic number 
-    #%%
+#%%
     for node in range(num_Nodes):
         G.add_node(node + 1); #one-base
 
-    #%% 
+#%% 
     num_Edges = randint(1, num_Nodes * (num_Nodes - 1)/2); 
+    f= open("test.dot", "w+")
+    f.write("digraph g{\n rankdir = LR\n")
 
     # generate some edge
     for _ in range(num_Edges):
@@ -23,15 +25,16 @@ def flow_network_generator():
         while to_Node == from_Node:
             to_Node = randint(1, num_Nodes); 
         edge_weight = randint(1, 100); #magic number
-        G.add_weighted_edges_from([(from_Node, to_Node, edge_weight)])
+        G.add_edge(from_Node, to_Node, capacity = edge_weight, flow = 0)
+        f.write("%d -> %d [label = \" %d \"];\n" % (from_Node , to_Node , edge_weight))
+    f.write("}\n")
+    f.close()
+#     return G
+# def max_flow_generator(G, s, t):
+#%%
+    s = 1
+    t = 2
 
-    #%%
-    # plt.subplot(121)
-    # nx.draw(G, with_labels=True, font_weight='bold')
-    # plt.show()
-
-    return G
-def max_flow_generator():
 
 
 # %%
